8127b8b4b841b090d6186b7057e3b307143ab595
LadaLex0 <91010364+LadaLex0@users.noreply.github.com>
2021-09-19 16:21:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-
+a = float(input('Write the 1st number'))
+b = float(input('Write the 2nd number'))
+s = a + b
+p = a * b
+d = a - b
+q = a / b
+print('The sum is ', s)
+print('The product is ', p)
+print('The difference is ', d)
+print('The quotient is ', q)
 
